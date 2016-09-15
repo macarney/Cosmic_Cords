525c8b2636acab05d97557ab3a062aacc3290042
GAMINGPC\Michael Carney <michael@michaelacarney.com>
2016-09-14 20:48:18 -0400

    Floating Working Individual Sounds Working Error when audioSource.Stop called.

--- Assets/Entities/Constellation/TriggerByLooking.cs
+++ Assets/Entities/Constellation/TriggerByLooking.cs
@@ -5,18 +5,21 @@ public class TriggerByLooking : MonoBehaviour {  //changes a color of an object 
 												 // currently using this to test when an object knows it is seen / unseen.
 
 	public Transform rayObjectDir; 
-	public int maxHitDistnace = 1000;
+	public int maxHitDistnace = 100;
+
+    public GameObject ringPrefab;
 
 	private ChangeMaterial changeMaterial;	//tells this object to change colors if it is selected
 	private GameObject  lastObject;   
-    private AudioSource audioSource;        
+    private AudioSource audioSource;
+    public Float myFloat;
 
     public bool isSelected = false;
     public bool isPlayingCord = false;
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 	
 	void Update () 
@@ -25,7 +28,7 @@ public class TriggerByLooking : MonoBehaviour {  //changes a color of an object 
 
         if (!isPlayingCord)
         {
-            audioSource.Stop();
+            //audioSource.Stop();
         }
 	}
 
@@ -67,9 +70,14 @@ public class TriggerByLooking : MonoBehaviour {  //changes a color of an object 
         isSelected = true;
 
 		changeMaterial = hitObject.GetComponent<ChangeMaterial>();
-		changeMaterial.ChangeTo ("blue");
+        audioSource = hitObject.GetComponent<AudioSource>();
+        changeMaterial.ChangeTo ("blue");
 
 		lastObject = hitObject;
+        myFloat.isFloatingUp = true;
+        myFloat.isFloatingDown = false;
+
+        Instantiate(ringPrefab, new Vector3(hitObject.transform.localPosition.x, hitObject.transform.localPosition.y, hitObject.transform.localPosition.z), Quaternion.identity);
 
         isPlayingCord = true;
         if (isPlayingCord)
@@ -83,6 +91,8 @@ public class TriggerByLooking : MonoBehaviour {  //changes a color of an object 
 
     private void Deselect() //when the lat object that was selected is NOT hit by the raycast change color back to red
 	{
+        //audioSource = lastObject.GetComponent<AudioSource>();
+
         isSelected = false;
 		if (changeMaterial != null)
 		{
@@ -90,5 +100,7 @@ public class TriggerByLooking : MonoBehaviour {  //changes a color of an object 
 		}
         audioSource.Stop();
         isPlayingCord = false;
-	}
+        //myFloat.isFloatingUp = false;
+        //myFloat.isFloatingDown = true;
+    }
 }
---
+++ Assets/Float.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour
+{
+    public bool isFloatingUp;
+    public bool isFloatingDown;
+
+    public float floatSpeed;
+
+    public void Update()
+    {
+        if (isFloatingUp)
+        {
+            FloatUp();
+        }
+
+        if (isFloatingDown)
+        {
+            if (transform.position.y >=0)
+            {
+                FloatDown();
+            }
+        }
+    }
+    public void FloatUp()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime / floatSpeed, 0);
+    }
+
+    public void FloatDown()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime / floatSpeed, 0);
+    }
+}
